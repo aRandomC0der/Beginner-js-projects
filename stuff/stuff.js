d4363c72819d2834af1843f1ea665d628a0b40d8
aRandomC0der <aurelliushanz@gmail.com>
2021-10-21 11:14:21 +0800

    commit

--- stuff/stuff.js
+++
@@ -1,7 +0,0 @@
-setInterval(time, 1000)
-function time() {
-  let timeNow = new Date().toLocaleTimeString('en-us', { hour: 'numeric', minute: 'numeric', hour12: true });
-document.getElementById('current-time').innerHTML = timeNow;
-}
-
-time(); 
